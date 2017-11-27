61ab2867cc578c577e1260b515c30f09b5fbc966
Zink <MZink@graphisoft.com>
2017-11-27 11:01:21 +0100

    init

---
+++ DrawAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    class DrawAction
+    {
+        public char type { get; set; }
+        public Rectangle rect { get; set; }
+        public Color color { get; set; }
+        //.....
+
+        public DrawAction(char type_, Rectangle rect_, Color color_)
+        { type = type_; rect = rect_; color = color_; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+
+
+    public partial class Form1 : Form
+    {
+        bool started = false;
+        List<DrawAction> actions = new List<DrawAction>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            started = true;
+            this.Cursor = Cursors.Cross;
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (DrawAction da in actions)
+            {
+                if (da.type == 'R') e.Graphics.DrawRectangle(new Pen(da.color), da.rect);
+                else if (da.type == 'E') e.Graphics.DrawEllipse(new Pen(da.color), da.rect);
+                //..
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            actions.Clear();
+            panel1.Invalidate();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (started)
+            {
+                Pen blackPen = new Pen(Color.Black, 3);
+                actions.Add(new DrawAction('R', new Rectangle(e.X, e.Y, 100, 50), Color.DarkGoldenrod));
+                started = false;
+                this.Cursor = Cursors.Default;
+                panel1.Invalidate();
+            }
+        }
+    }
+}
